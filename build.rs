18e90cb080feaee28e794757918051ec52768433
soyuo <tasexyz@gmail.com>
2026-08-21 13:09:58 +0900

    fix: 인스톨러 바이너리 이름을 RendogLauncherInstaller 로 통일

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main() {
             .set("ProductName", "Rendog Launcher Installer")
             .set("FileDescription", "Rendog Launcher Installer")
             .set("CompanyName", "Rendog")
-            .set("OriginalFilename", "rendog-launcher-installer.exe");
+            .set("OriginalFilename", "RendogLauncherInstaller.exe");
         resource
             .compile()
             .expect("failed to compile Windows resources");
